62ec57eacf1b9c054afa25800519b90fd05f8771
Nikola <nikola@nursa.com>
2022-12-23 15:32:16 +0100

    made three json helper functions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dzonib/toolkit
+
+go 1.19
---
+++ toolkit.go
@@ -0,0 +1,133 @@
+package toolbox
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+// Tools is the type for this package. Create a variable of this type, and you have access
+// to all the exported methods with the receiver type *Tools.
+type Tools struct {
+	MaxJSONSize        int      // maximum size of JSON file we'll process
+	MaxFileSize        int      // maximum size of uploaded files in bytes
+	AllowedFileTypes   []string // allowed file types for upload (e.g. image/jpeg)
+	AllowUnknownFields bool     // if set to true, allow unknown fields in JSON
+}
+
+// JSONResponse is the type used for sending JSON around.
+type JSONResponse struct {
+	Error   bool        `json:"error"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+
+// ReadJSON tries to read the body of a request and converts it into JSON.
+func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	maxBytes := 1024 * 1024 // one megabyte
+
+	// if MaxJSONSize is set, use that value instead of default
+	if t.MaxJSONSize != 0 {
+		maxBytes = t.MaxJSONSize
+	}
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+
+	// should we allow unknown fields?
+	if !t.AllowUnknownFields {
+		dec.DisallowUnknownFields()
+	}
+
+	// attempt to decode the data, and figure out what the error is, so as to send back a human readable
+	// response
+	err := dec.Decode(data)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
+
+		case errors.As(err, &invalidUnmarshalError):
+			return fmt.Errorf("error unmarshalling json: %s", err.Error())
+
+		default:
+			return err
+		}
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
+
+	return nil
+}
+
+// WriteJSON takes a response status code and arbitrary data and writes a json response to the client.
+func (t *Tools) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// if we have a value as the last parameter in the function call, then we are setting a custom header.
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	// set the content type and send response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ErrorJSON takes an error, and optionally a response status code, and generates and sends
+// a json error response.
+func (t *Tools) ErrorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	// if a custom response code is specified, use that instead of bad request
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload JSONResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return t.WriteJSON(w, statusCode, payload)
+}
